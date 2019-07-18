365240a1afb1685b9172b39ad6e9b3c0cf5cd9dd
Anais Momin Ahmed <anaismahmed@uchicago.edu>
2019-07-18 01:56:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language|
+    language.each do |name, typehash|
+      if new_hash.key?(name)
+        new_hash[name][:style] << style
+      else 
+        stylearray = []
+        datahash = typehash
+        stylearray << style
+        datahash[:style] = stylearray
+        new_hash[name] = datahash
+      end
+    end
+  end
+  new_hash
 end
